49703fbed2d04a9191ce04987a4a17eed36165e6
Pavlo <pavlo.rohozhyn@gmail.com>
2025-02-22 08:45:09 +0100

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
 
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const { email, password } = e.target.elements;
+    const emailData = email.value.trim();
+    const passData = password.value.trim();
+    if (!emailData || !passData) {
+        alert("All form fields must be filled in");
+        return;
+    }
+    console.log({
+        email: emailData,
+        password: passData
+    });
+    loginForm.reset();
+});
+
