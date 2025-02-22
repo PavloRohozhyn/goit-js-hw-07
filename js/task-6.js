3a4e5988f180b92d387388af62aefe640a9bab78
Pavlo <pavlo.rohozhyn@gmail.com>
2025-02-22 13:29:16 +0100

    task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputControl = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const boxContainer = document.getElementById('boxes');
+
+function createBoxes(amount) {
+  boxContainer.innerHTML = ''; // clear
+  for (let i = 0; i < amount; i++) {
+    const size = 30 + i * 10; // element size
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxContainer.innerHTML = ''; // clear block
+}
+
+createBtn.addEventListener('click', () => {
+  const amount = Number(inputControl.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  }
+  inputControl.value = ''; // clear text
+});
+
+document.querySelector('[data-destroy]').addEventListener('click', destroyBoxes);
